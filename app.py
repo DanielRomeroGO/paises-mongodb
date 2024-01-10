fb6bc6a81341548e97b7cec323117c6f0d177a4e
Dani <ploejem38@gmail.com>
2024-01-10 11:35:18 +0100

    first commit

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_pymongo import PyMongo # Necesita el paquete Flask-PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
 from bson import json_util
 from bson.objectid import ObjectId
+from datetime import date, datetime
 
 app = Flask(__name__)
 
@@ -14,59 +15,71 @@ app.config["MONGO_URI"] = 'mongodb+srv://daniromero:palomeras@cluster0.k5qwzob.m
 mongo = PyMongo(app)
 
 #obtiene todos los usuarios
-@app.route('/users', methods=['GET'])
-def get_users():
-    usuarios = mongo.db.users.find()
-    response = json_util.dumps(usuarios) # Strings con formato JSON
+@app.route('/paises', methods=['GET'])
+def get_paises():
+    paises = mongo.db.paises.find()
+    response = json_util.dumps(paises) # Strings con formato JSON
 
     return Response(response, mimetype='application/json') # Formato JSON
 
 #busca un usuario por id
-@app.route('/users/<id>', methods=['GET'])
-def get_user(id):
-    usuario = mongo.db.users.find_one({'_id': ObjectId(id)})
-    if usuario:
-        response = json_util.dumps(usuario) # Strings con formato JSON
+@app.route('/paises/<id>', methods=['GET'])
+def get_pais(id):
+    paises = mongo.db.paises.find_one({'_id': ObjectId(id)})
+    if paises:
+        response = json_util.dumps(paises) # Strings con formato JSON
 
         return Response(response, mimetype='application/json') # Formato JSON
 
 #crea usuarios
-@app.route('/users', methods=['POST'])
-def create_user():
+@app.route('/paises', methods=['POST'])
+def create_pais():
     request_data = request.get_json()
     # Comprobando que se ha cada uno de los datos
-    if 'nomusuario' in request_data:
-        nomusuario = request.json['nomusuario']
+    if 'nompais' in request_data:
+        nompais = request.json['nompais']
     else:
         return datos_incompletos()
-    if 'email' in request_data:
-        email = request.json['email']
+    if 'capital' in request_data:
+        capital = request.json['capital']
     else:
         return datos_incompletos()
-    if 'passw' in request_data:
-        passw = request.json['passw']
-        hashed_password = generate_password_hash(passw)
+    if 'habitantes' in request_data:
+        habitantes_str = request.json['habitantes']
+        if not habitantes_str.isdigit():
+            return habitantes_no_valido()
+        habitantes = int(habitantes_str)
     else:
         return datos_incompletos()
     
-    id = mongo.db.users.insert_one({'nomusuario': nomusuario, 'email':
-    email, 'passw': hashed_password})
+    if 'diaNacional' in request_data:
+        diaNacional_str = request.json['diaNacional']
+        try:
+            diaNacional = datetime.strptime(diaNacional_str, '%Y-%m-%d').date()
+        except ValueError:
+            return dia_nacional_no_valido()
+    else:
+        return datos_incompletos()
+    
+    id = mongo.db.paises.insert_one({'nompais': nompais, 'capital':
+    capital, 'habitantes': habitantes, "diaNacional": diaNacional})
     response = {
     'id': str(id),
-    'nomusuario': nomusuario,
-    'passw': hashed_password,
-    'email': email
+    'nompais': nompais,
+    'capital': capital,
+    'habitantes': habitantes,
+    "diaNacional": diaNacional
     }
     return response
 
 #elimina usuarios
-@app.route('/users/<id>', methods=['DELETE'])
-def delete_user(id):
-    usuario = mongo.db.users.find_one({'_id': ObjectId(id)})
+@app.route('/paises/<id>', methods=['DELETE'])
+def delete_pais(id):
+    paises = mongo.db.paises.find_one({'_id': ObjectId(id)})
 
-    if usuario: # Encontrado para ser eliminado
-        usuarioborrar = mongo.db.users.delete_one({'_id': ObjectId(id)})
-        response = jsonify({'mensaje': 'Usuario ' + id + 'fue eliminado satisfactoriamente'})
+    if paises: # Encontrado para ser eliminado
+        paisborrar = mongo.db.paises.delete_one({'_id': ObjectId(id)})
+        response = jsonify({'mensaje': 'Pais ' + id + 'fue eliminado satisfactoriamente'})
         return response
     else:
         return not_found()
@@ -80,37 +93,47 @@ def not_found(error=None):
     return response
 
 #modifica usuarios
-@app.route('/users/<id>', methods=['PUT'])
-def update_user(id):
+@app.route('/paises/<id>', methods=['PUT'])
+def update_pais(id):
     request_data = request.get_json()
     # Comprobando que se ha cada uno de los datos
-    if 'nomusuario' in request_data:
-        nomusuario = request.json['nomusuario']
+    if 'nompais' in request_data:
+        nompais = request.json['nompais']
+    else:
+        return datos_incompletos()
+    if 'capital' in request_data:
+        capital = request.json['capital']
+    else:
+        return datos_incompletos()
+    if 'habitantes' in request_data:
+        habitantes_str = request.json['habitantes']
+        if not habitantes_str.isdigit():
+            return habitantes_no_valido()
+        habitantes = int(habitantes_str)
     else:
         return datos_incompletos()
     
-    if 'email' in request_data:
-        email = request.json['email']
+    if 'diaNacional' in request_data:
+        diaNacional_str = request.json['diaNacional']
+        try:
+            diaNacional = datetime.strptime(diaNacional_str, '%Y-%m-%d').date()
+        except ValueError:
+            return dia_nacional_no_valido()
     else:
         return datos_incompletos()
     
-    if 'passw' in request_data:
-        passw = request.json['passw']
-        hashed_password = generate_password_hash(passw)
-    else:
-        return datos_incompletos()
-    
-    usuario = mongo.db.users.find_one({'_id': ObjectId(id)})
-    if usuario: # Encontrado para ser modificado
-        mongo.db.users.update_one({'_id': ObjectId(id)}, {'$set':
+    pais = mongo.db.paises.find_one({'_id': ObjectId(id)})
+    if pais: # Encontrado para ser modificado
+        mongo.db.paises.update_one({'_id': ObjectId(id)}, {'$set':
         {
-        'nomusuario': nomusuario,
-        'passw': hashed_password,
-        'email': email
+        'nompais': nompais,
+        'capital': capital,
+        'habitantes': habitantes,
+        "diaNacional": diaNacional
         }})
     else:
         return not_found()
-    response = jsonify({'mensaje': 'Usuario ' + id + 'fue actualizado satisfactoriamente'})
+    response = jsonify({'mensaje': 'Pais ' + id + 'fue actualizado satisfactoriamente'})
     return response
 
 @app.errorhandler(404)
@@ -124,10 +147,28 @@ def not_found(error=None):
 @app.errorhandler(400)
 def datos_incompletos(error=None):
     response = jsonify({
-        'mensaje': 'Datos incompletos: nomusuario, email y/o passw',
+        'mensaje': 'Datos incompletos',
         'status': 400
     })
     response.status_code = 400
+    return response
+
+@app.errorhandler(422)
+def habitantes_no_valido(error=None):
+    response = jsonify({
+        'mensaje': 'Habitantes debe ser un número entero',
+        'status': 422
+    })
+    response.status_code = 422
+    return response
+
+@app.errorhandler(422)
+def dia_nacional_no_valido(error=None):
+    response = jsonify({
+        'mensaje': 'Formato de diaNacional no válido. Utilice el formato YYYY-MM-DD',
+        'status': 422
+    })
+    response.status_code = 422
     return response
 
 if __name__ == "__main__":
